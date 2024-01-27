4e5ac84ee0c54d2f1bab19abd21fad6ec57eddae
Muzi Phage <muzi.phage@gmail.com>
2024-01-27 12:52:32 +0200

    Initial commit

---
+++ src/main/java/com/example/ec/service/TourPackageService.java
@@ -0,0 +1,31 @@
+package com.example.ec.service;
+
+import com.example.ec.domain.TourPackage;
+import com.example.ec.repo.TourPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourPackageService {
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    // Check if the TourPackage with the given code exists else create the new one
+    // with provided params and save it to the repo
+    public TourPackage createTourPackage(String code, String name) {
+        return tourPackageRepository.findById(code).orElse(tourPackageRepository.save(new TourPackage(code, name)));
+    }
+
+    // Look up all tour packages
+    public Iterable<TourPackage> lookup() {
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+}
